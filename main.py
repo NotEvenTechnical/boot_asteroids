76cd3853ca7b47564586a9234e816802bddb19a6
NotEvenTechnical <208291839+NotEvenTechnical@users.noreply.github.com>
2025-05-08 18:45:59 +0200

    00.00.00.01: added clock (60 FPS hard cap)

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,6 +18,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = ( clock.tick( 60 ) ) / 1000    # measure the dt in seconds
         #moar stuff to all
 
 

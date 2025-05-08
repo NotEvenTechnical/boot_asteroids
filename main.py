9ed8e9d683109c88ecf30d00f0e07c373b9de42c
NotEvenTechnical <208291839+NotEvenTechnical@users.noreply.github.com>
2025-05-08 19:43:03 +0200

    00.00.00.02: added player + draw player shape + circle hitbox

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
+
 from constants import *
+from player import Player
+
 
 def main():
     print ("Starting Asteroids!")
@@ -11,12 +14,17 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player( x = SCREEN_WIDTH / 2 , y = SCREEN_HEIGHT / 2 )
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+
+        player.draw(screen)
+
         pygame.display.flip()
 
         dt = ( clock.tick( 60 ) ) / 1000    # measure the dt in seconds
